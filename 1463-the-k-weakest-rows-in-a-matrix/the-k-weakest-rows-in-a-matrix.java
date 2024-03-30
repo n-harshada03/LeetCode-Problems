71714c2938429d89b7e05c447e2370ad4668c584
n-harshada03 <127178290+n-harshada03@users.noreply.github.com>
2024-03-30 12:01:30 +0530

    Time: 3 ms (53.50%) | Memory: 44.8 MB (40.36%) - LeetSync

---
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -0,0 +1,40 @@
+class Solution {
+    public class Row implements Comparable<Row>{
+        int soldiers;
+        int idx;
+
+        public Row(int soldiers, int idx){
+            this.soldiers=soldiers;
+            this.idx=idx;
+        }
+
+        @Override
+        public int compareTo( Row r2){
+            if(this.soldiers==r2.soldiers){
+                return this.idx-r2.idx;
+            }
+            else{
+                return this.soldiers-r2.soldiers;
+            }
+        }
+    }
+    public int[] kWeakestRows(int[][] mat, int k) {
+
+        PriorityQueue<Row> pq=new PriorityQueue<>();
+        for(int i=0; i<mat.length;i++){
+            int count=0;
+            for(int j=0; j< mat[0].length;j++){
+                if (mat[i][j]==1){
+                    count++;
+                }
+            }
+            pq.add(new Row(count, i));
+        }
+
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = pq.remove().idx;
+        }
+        return result;
+    }
+}
