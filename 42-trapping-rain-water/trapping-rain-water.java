60403c5e3a7a94c5da47edc12875fac73229f6ff
n-harshada03 <127178290+n-harshada03@users.noreply.github.com>
2024-07-06 11:29:20 +0530

    Time: 1 ms (69.65%) | Memory: 45.6 MB (88.87%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,27 +1,24 @@
 class Solution {
     public int trap(int[] height) {
         int n=height.length;
-        if(n<=2){
-            return 0;
-        }
-        
-        int result =0;
-        int[] rightmax=new int [n];
-        int[] leftmax=new int [n];
+        int leftMax[]=new int[n];
+        int rightMax[]=new int[n];
+        int count=0;
 
-        leftmax[0]=height[0];
+        leftMax[0]=height[0];
         for(int i=1;i<n;i++){
-            leftmax[i]=Math.max(leftmax[i-1] ,height[i]);
+            leftMax[i]=Math.max(leftMax[i-1],height[i]);
         }
 
-        rightmax[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--){
-            rightmax[i]=Math.max(rightmax[i+1] ,height[i]);
+        rightMax[n-1]=height[n-1];
+        for (int j=n-2;j>=0;j--){
+            rightMax[j]=Math.max(rightMax[j+1],height[j]);
         }
 
-        for(int i=0;i<n;i++){
-            result += Math.min(leftmax[i], rightmax[i]) - height[i];
+        for(int i=1;i<n;i++){
+            count=count+ (Math.min(leftMax[i],rightMax[i]))-height[i];
         }
-    return result;
+
+        return count;
     }
 }
