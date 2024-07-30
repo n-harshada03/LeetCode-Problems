ddc5c1b25b549399362181849f53933b08f81b09
n-harshada03 <127178290+n-harshada03@users.noreply.github.com>
2024-07-30 10:51:05 +0530

    Time: 3 ms (88.77%) | Memory: 44.8 MB (10.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -5,11 +5,9 @@ class Solution {
 
         Arrays.sort(array1);
         Arrays.sort(array2);
-        
+
         String s1=new String(array1);
-        System.out.println(s1);
         String s2=new String(array2);
-        System.out.println(s2);
 
         if(s1.equals(s2)){
             return true;
